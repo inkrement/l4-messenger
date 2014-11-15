a6c6f13f6be220eb0670b1da9e82b0e8c090c2c9
Mattias Fjellvang <mattias@fjellvang.dk>
2014-11-15 20:17:34 +0100

    removed users migration, removed user models non-message methods, removed token usertable migration

--- src/models/User.php
+++ src/models/User.php
@@ -3,68 +3,7 @@ namespace Pichkrement\Messenger\Models;
 
 use Pichkrement\Messenger\Models\Conversation as Conversation;
 
-class User extends \Eloquent implements \Illuminate\Auth\UserInterface, \Illuminate\Auth\Reminders\RemindableInterface {
-
-	protected $guarded = array('id', 'password');
-
-	//enable timestamps
-	public $timestamps = true;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
-	protected $table = 'users';
-
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'token');
-
-	protected $fillable = array('username', 'firstname', 'surname', 'email', "password", "token");
-
-	public static $rules = array(
-		"username" => 'required|min:4|unique:users',
-		'surname' => 'Required|Min:3|Max:80',
-		'firstname' => 'Required|Min:3|Max:80',
-		'email' => 'required|between:3,64|email|unique:users',
-		'password' => 'required',
-		'token' => 'required|min:3'
-	);
-		
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
+class User extends \Eloquent {
 
 
 	public function messages(){
